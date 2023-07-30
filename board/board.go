6576d1049b9dce6788d76096c6a8e1da9f3ca166
j1642 <60148902+j1642@users.noreply.github.com>
2023-07-29 21:38:25 -0400

    MAINT: Move attack bitboard precalculation to board.go

--- board/board.go
+++ board/board.go
@@ -42,3 +42,165 @@ func New() *Board {
 		bKing:    uint64(1 << 60),
 	}
 }
+
+func getFiles() [4][8]int {
+	fileA, fileB, fileG, fileH := [8]int{}, [8]int{}, [8]int{}, [8]int{}
+
+	for i := 0; i < 8; i++ {
+		fileA[i] = i * 8
+		fileB[i] = i*8 + 1
+		fileG[i] = i*8 + 6
+		fileH[i] = i*8 + 7
+	}
+
+	return [4][8]int{fileA, fileB, fileG, fileH}
+}
+
+/*
+fuenc getFiles() [4]map[int]bool {
+    When only making knight BBs, maps are slower. Keep in case maps become
+    faster when used for more pieces.
+
+    fileA, fileB, fileG, fileH := make(map[int]bool, 8), make(map[int]bool, 8),
+        make(map[int]bool, 8), make(map[int]bool, 8)
+
+    for i := 0; i < 8; i++ {
+        fileA[i*8] = true
+        fileB[i*8+1] = true
+        fileG[i*8+6] = true
+        fileH[i*8+7] = true
+    }
+
+    return [4]map[int]bool{fileA, fileB, fileG, fileH}
+}
+*/
+
+func containsN(n int, nums [8]int) bool {
+	for _, num := range nums {
+		if n == num {
+			return true
+		}
+	}
+	return false
+}
+
+func MakeKnightBBs() [64]uint64 {
+	bbs := [64]uint64{}
+	directions := []int{}
+	files := getFiles()
+
+	for sq := 0; sq < 64; sq++ {
+		switch {
+		case containsN(sq, files[0]):
+			directions = []int{17, 10, -6, -15}
+		case containsN(sq, files[1]):
+			directions = []int{17, 15, 10, -6, -17, -15}
+		case containsN(sq, files[2]):
+			directions = []int{17, 15, -17, -15, 6, -10}
+		case containsN(sq, files[3]):
+			directions = []int{15, -17, 6, -10}
+		default:
+			directions = []int{17, 15, 10, -6, -17, -15, 6, -10}
+		}
+		/*
+		   190ms bin search vs 180ms linear search
+		   case binSearch(sq, files[0]):
+		       directions = []int{17, 10, -6, -15}
+		   case binSearch(sq, files[1]):
+		       directions = []int{17, 15, 10, -6, -17, -15}
+		   case binSearch(sq, files[2]):
+		       directions = []int{17, 15, -17, -15, 6, -10}
+		   case binSearch(sq, files[3]):
+		       directions = []int{15, -17, 6, -10}
+		*/
+
+		/*
+		   When only making knight BBs, maps are slower. Keep to check if maps are
+		   faster when used for more pieces.
+
+		   if  _, ok := files[0][sq]; ok {
+		       directions = []int{17, 10, -6, -15}
+		   } else if _, ok := files[1][sq]; ok {
+		       directions = []int{17, 15, 10, -6, -17, -15}
+		   } else if _, ok := files[2][sq]; ok {
+		       directions = []int{17, 15, -17, -15, 6, -10}
+		   } else if _, ok := files[3][sq]; ok {
+		       directions = []int{15, -17, 6, -10}
+		   } else {
+		       directions = []int{17, 15, 10, -6, -17, -15, 6, -10}
+		   }
+		*/
+
+		for _, d := range directions {
+			if sq+d < 0 || sq+d > 63 {
+				continue
+			}
+			bbs[sq] += 1 << (sq + d)
+		}
+	}
+
+	return bbs
+}
+
+func makeKingBBs() [64]uint64 {
+	bbs := [64]uint64{}
+	directions := []int{}
+	files := getFiles()
+
+	for sq := 0; sq < 64; sq++ {
+		switch {
+		// file A
+		case containsN(sq, files[0]):
+			directions = []int{8, 9, 1, -7, -8}
+		// file H
+		case containsN(sq, files[3]):
+			directions = []int{8, 7, -1, -9, -8}
+		default:
+			directions = []int{7, 8, 9, -1, 1, -9, -8, -7}
+		}
+
+		for _, d := range directions {
+			if sq+d < 0 || sq+d > 63 {
+				continue
+			}
+			bbs[sq] += 1 << (sq + d)
+		}
+	}
+
+	return bbs
+}
+
+func makeSlidingAttackBBs() [8][64]uint64 {
+	bbs := [8][64]uint64{}
+	files := getFiles()
+	// TODO: make containsN() generic to remove wasted zeroes.
+	// Or use slices instead of arrays.
+	fileAForbidden := [8]int{-9, -1, 7, 0, 0, 0, 0, 0}
+	fileHForbidden := [8]int{9, 1, -7, 0, 0, 0, 0, 0}
+
+	// Movement directions are ordered clockwise.
+	for i, dir := range [8]int{8, 9, 1, -7, -8, -9, -1, 7} {
+		for sq := 0; sq < 64; sq++ {
+			if containsN(sq, files[0]) && containsN(dir, fileAForbidden) {
+				continue
+			} else if containsN(sq, files[3]) && containsN(dir, fileHForbidden) {
+				continue
+			}
+
+			for j := 1; j < 8; j++ {
+				newSq := j*dir + sq
+				if newSq < 0 || newSq > 63 {
+					break
+				}
+				bbs[i][sq] += 1 << newSq
+				// Found board edge
+				if dir != 8 && dir != -8 &&
+					(containsN(newSq, files[0]) || containsN(newSq, files[3])) {
+					break
+				}
+			}
+		}
+	}
+
+	return bbs
+}
--- board/board_test.go
+++ board/board_test.go
@@ -1,10 +1,11 @@
 package board
 
 import (
+	"math"
 	"testing"
 )
 
-// cb = chessboard, w = white, b = black
+// cb = chessboard, w = white, b = black, bb = bitboard
 
 func TestNew(t *testing.T) {
 	cb := New()
@@ -42,4 +43,223 @@ func TestNew(t *testing.T) {
 		t.Errorf("bPieces != union of all black pieces. want=%b,\ngot=%b",
 			uint64(1)<<63-1+uint64(1)<<63-(uint64(1)<<48-1), bPiecesUnion)
 	}
+}
+
+type bbTestCase struct {
+	square   int
+	expected uint64
+	actual   uint64
+	name     string
+}
+
+func runMoveBBTests(t *testing.T, tests []bbTestCase) {
+	for _, tt := range tests {
+		if tt.actual != tt.expected {
+			t.Errorf("incorrect bitboard moves for a %v on square %d.\nexpected:\n%b, %T\ngot:\n%b, %T",
+				tt.name, tt.square, tt.expected, tt.expected, tt.actual, tt.actual)
+		}
+	}
+}
+
+func TestKnightBBs(t *testing.T) {
+	bbs := MakeKnightBBs()
+
+	tests := []bbTestCase{
+		{
+			// file A
+			square:   0,
+			expected: uint64(math.Pow(2, 17) + math.Pow(2, 10)),
+			actual:   bbs[0],
+			name:     "knight",
+		},
+		{
+			// file B
+			square: 57,
+			expected: uint64(math.Pow(2, 57-17) + math.Pow(2, 57-15) +
+				1<<(57-6)),
+			actual: bbs[57],
+			name:   "knight",
+		},
+		{
+			// file G
+			square: 6,
+			expected: uint64(math.Pow(2, 6+17) + math.Pow(2, 6+15) +
+				1<<(6+6)),
+			actual: bbs[6],
+			name:   "knight",
+		},
+		{
+			// file H
+			square: 55,
+			expected: uint64(math.Pow(2, 55-17) + math.Pow(2, 55+6) +
+				1<<(55-10)),
+			actual: bbs[55],
+			name:   "knight",
+		},
+		{
+			// center of board
+			square: 20,
+			expected: uint64(math.Pow(2, 20+17) + math.Pow(2, 20+15) +
+				math.Pow(2, 20-17) + math.Pow(2, 20-15) + 1<<(20-10) +
+				math.Pow(2, 20+6) + math.Pow(2, 20+10) + math.Pow(2, 20-6)),
+			actual: bbs[20],
+			name:   "knight",
+		},
+	}
+
+	runMoveBBTests(t, tests)
+}
+
+func TestKingBBs(t *testing.T) {
+	bbs := makeKingBBs()
+	tests := []bbTestCase{
+		{
+			square:   0,
+			expected: uint64(1<<8 + 1<<9 + 1<<1),
+			actual:   bbs[0],
+			name:     "king",
+		},
+		{
+			square:   7,
+			expected: uint64(1<<(7-1) + 1<<(7+8) + 1<<(7+7)),
+			actual:   bbs[7],
+			name:     "king",
+		},
+		{
+			square: 1,
+			expected: uint64(1<<(1-1) +
+				1<<(1+1) +
+				1<<(1+7) +
+				1<<(1+8) +
+				1<<(1+9)),
+			actual: bbs[1],
+			name:   "king",
+		},
+		{
+			square: 62,
+			expected: uint64(1)<<(62+1) +
+				uint64(1)<<(62-1) +
+				uint64(1)<<(62-9) +
+				uint64(1)<<(62-8) +
+				uint64(1)<<(62-7),
+			actual: bbs[62],
+			name:   "king",
+		},
+	}
+
+	runMoveBBTests(t, tests)
+}
+
+func TestSlidingAttackBBs(t *testing.T) {
+	bbs := makeSlidingAttackBBs()
+	tests := []bbTestCase{
+		{
+			square: 0,
+			expected: uint64(1)<<8 +
+				uint64(1)<<16 +
+				uint64(1)<<24 +
+				uint64(1)<<32 +
+				uint64(1)<<40 +
+				uint64(1)<<48 +
+				uint64(1)<<56,
+			// Direction 0 is north, 1 is northeast, etc.
+			actual: bbs[0][0],
+			name:   "N ray",
+		},
+		{
+			square: 0,
+			expected: uint64(1)<<9 +
+				uint64(1)<<18 +
+				uint64(1)<<27 +
+				uint64(1)<<36 +
+				uint64(1)<<45 +
+				uint64(1)<<54 +
+				uint64(1)<<63,
+			actual: bbs[1][0],
+			name:   "NE ray",
+		},
+		{
+			square: 0,
+			expected: uint64(1)<<1 +
+				uint64(1)<<2 +
+				uint64(1)<<3 +
+				uint64(1)<<4 +
+				uint64(1)<<5 +
+				uint64(1)<<6 +
+				uint64(1)<<7,
+			actual: bbs[2][0],
+			name:   "E ray",
+		},
+		{
+			square:   0,
+			expected: uint64(0),
+			actual:   bbs[3][0],
+			name:     "SE ray",
+		},
+		{
+			square: 9,
+			expected: uint64(1)<<17 +
+				uint64(1)<<25 +
+				uint64(1)<<33 +
+				uint64(1)<<41 +
+				uint64(1)<<49 +
+				uint64(1)<<57,
+			actual: bbs[0][9],
+			name:   "N ray",
+		},
+		{
+			square: 9,
+			expected: uint64(1)<<18 +
+				uint64(1)<<27 +
+				uint64(1)<<36 +
+				uint64(1)<<45 +
+				uint64(1)<<54 +
+				uint64(1)<<63,
+			actual: bbs[1][9],
+			name:   "NE ray",
+		},
+		{
+			square: 9,
+			expected: uint64(1)<<10 +
+				uint64(1)<<11 +
+				uint64(1)<<12 +
+				uint64(1)<<13 +
+				uint64(1)<<14 +
+				uint64(1)<<15,
+			actual: bbs[2][9],
+			name:   "E ray",
+		},
+		{
+			square:   9,
+			expected: uint64(1) << 2,
+			actual:   bbs[3][9],
+			name:     "SE ray",
+		},
+		{
+			square:   9,
+			expected: uint64(1) << 1,
+			actual:   bbs[4][9],
+			name:     "S ray",
+		},
+		{
+			square:   9,
+			expected: uint64(1),
+			actual:   bbs[5][9],
+			name:     "SW ray",
+		},
+		{
+			square:   9,
+			expected: uint64(1) << 8,
+			actual:   bbs[6][9],
+			name:     "W ray",
+		},
+		{
+			square:   9,
+			expected: uint64(1) << 16,
+			actual:   bbs[7][9],
+			name:     "NW ray",
+		},
+	}
+
+	runMoveBBTests(t, tests)
 }
