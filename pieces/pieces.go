48b11603b8ced40593c6d615acc85d5d92d2bef1
j1642 <60148902+j1642@users.noreply.github.com>
2023-07-29 22:43:11 -0400

    ENH: Add rook move generation with preliminary test

--- pieces/pieces.go
+++ pieces/pieces.go
@@ -1,14 +1,53 @@
 package pieces
 
 import (
+	"engine2/board"
 	_ "fmt"
 	_ "math"
+	"math/bits"
 )
 
-// bb = bit board
+/* bb = bit board, cb = chessboard
 
-//func findRookMoves(square int, cb *board.Board) uint64 {
-//}
+Positive direction move generation uses bitscan-forward or bits.TrailingZeros(),
+and the opposite for negative directions.
+*/
+
+// TODO: Investigate performance impact of branching in move gen.
+
+// Captures and protection are included in move gen.
+func getRookMoves(square int, cb *board.Board) uint64 {
+	// North
+	moves := cb.SlidingAttacks[0][square]
+	blockers := cb.SlidingAttacks[0][square] & (cb.WPieces | cb.BPieces)
+	if blockers != 0 {
+		blockerSq := bits.TrailingZeros64(blockers)
+		moves ^= cb.SlidingAttacks[0][blockerSq]
+	}
+	// East
+	moves |= cb.SlidingAttacks[2][square]
+	blockers = cb.SlidingAttacks[2][square] & (cb.WPieces | cb.BPieces)
+	if blockers != 0 {
+		blockerSq := bits.TrailingZeros64(blockers)
+		moves ^= cb.SlidingAttacks[2][blockerSq]
+	}
+	// South
+	moves |= cb.SlidingAttacks[4][square]
+	blockers = cb.SlidingAttacks[4][square] & (cb.WPieces | cb.BPieces)
+	if blockers != 0 {
+		blockerSq := 63 - bits.LeadingZeros64(blockers)
+		moves ^= cb.SlidingAttacks[4][blockerSq]
+	}
+	// West
+	moves |= cb.SlidingAttacks[6][square]
+	blockers = cb.SlidingAttacks[6][square] & (cb.WPieces | cb.BPieces)
+	if blockers != 0 {
+		blockerSq := 63 - bits.LeadingZeros64(blockers)
+		moves ^= cb.SlidingAttacks[6][blockerSq]
+	}
+
+	return moves
+}
 
 func binSearch(n int, nums [8]int) bool {
 	l := 0
--- pieces/pieces_test.go
+++ pieces/pieces_test.go
@@ -1,8 +1,34 @@
 package pieces
 
 import (
+	"engine2/board"
 	"testing"
 )
+
+type moveTestCase struct {
+	expected uint64
+	actual   uint64
+}
+
+func runMoveGenTests(t *testing.T, tests []moveTestCase) {
+	for _, tt := range tests {
+		if tt.expected != tt.actual {
+			t.Errorf("want:%b\n, got=%b", tt.expected, tt.actual)
+		}
+	}
+}
+
+func TestRookMoves(t *testing.T) {
+	cb := board.New()
+	tests := []moveTestCase{
+		{
+			expected: uint64(1<<1 + 1<<8),
+			actual:   getRookMoves(0, cb),
+		},
+	}
+
+	runMoveGenTests(t, tests)
+}
 
 func TestBinSearch(t *testing.T) {
 	nums := [8]int{0, 0, 0, 1, 2, 3, 4, 5}
