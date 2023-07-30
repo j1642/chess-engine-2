9638dafd62dd301f17e36a669565898ccc40aefd
j1642 <60148902+j1642@users.noreply.github.com>
2023-07-29 22:04:23 -0400

    MAINT: Make board struct attributes public

--- board/board.go
+++ board/board.go
@@ -2,44 +2,52 @@ package board
 
 type Board struct {
 	// TODO: Avoid branching by using knights[wToMove][square]
-	wToMove int // 1 or 0, true or false
+	WToMove int // 1 or 0, true or false
 
-	wPieces  uint64
-	wPawns   uint64
-	wKnights uint64
-	wBishops uint64
-	wRooks   uint64
-	wQueens  uint64
-	wKing    uint64
+	WPieces  uint64
+	WPawns   uint64
+	WKnights uint64
+	WBishops uint64
+	WRooks   uint64
+	WQueens  uint64
+	WKing    uint64
 
-	bPieces  uint64
-	bPawns   uint64
-	bKnights uint64
-	bBishops uint64
-	bRooks   uint64
-	bQueens  uint64
-	bKing    uint64
+	BPieces  uint64
+	BPawns   uint64
+	BKnights uint64
+	BBishops uint64
+	BRooks   uint64
+	BQueens  uint64
+	BKing    uint64
+
+	NAttacks       [64]uint64
+	KAttacks       [64]uint64
+	SlidingAttacks [8][64]uint64
 }
 
 func New() *Board {
 	return &Board{
-		wToMove: 1,
+		WToMove: 1,
 
-		wPieces:  uint64(1)<<16 - 1,
-		wPawns:   uint64(1)<<16 - 1 - (uint64(1)<<8 - 1),
-		wKnights: uint64(1<<1 + 1<<6),
-		wBishops: uint64(1<<2 + 1<<5),
-		wRooks:   uint64(1<<0 + 1<<7),
-		wQueens:  uint64(1 << 3),
-		wKing:    uint64(1 << 4),
+		WPieces:  uint64(1)<<16 - 1,
+		WPawns:   uint64(1)<<16 - 1 - (uint64(1)<<8 - 1),
+		WKnights: uint64(1<<1 + 1<<6),
+		WBishops: uint64(1<<2 + 1<<5),
+		WRooks:   uint64(1<<0 + 1<<7),
+		WQueens:  uint64(1 << 3),
+		WKing:    uint64(1 << 4),
 
-		bPieces:  uint64(1)<<63 - 1 + uint64(1)<<63 - (uint64(1)<<48 - 1),
-		bPawns:   uint64(1)<<56 - 1 - (uint64(1)<<48 - 1),
-		bKnights: uint64(1<<57 + 1<<62),
-		bBishops: uint64(1<<58 + 1<<61),
-		bRooks:   uint64(1<<56 + 1<<63),
-		bQueens:  uint64(1 << 59),
-		bKing:    uint64(1 << 60),
+		BPieces:  uint64(1)<<63 - 1 + uint64(1)<<63 - (uint64(1)<<48 - 1),
+		BPawns:   uint64(1)<<56 - 1 - (uint64(1)<<48 - 1),
+		BKnights: uint64(1<<57 + 1<<62),
+		BBishops: uint64(1<<58 + 1<<61),
+		BRooks:   uint64(1<<56 + 1<<63),
+		BQueens:  uint64(1 << 59),
+		BKing:    uint64(1 << 60),
+
+		NAttacks:       MakeKnightBBs(),
+		KAttacks:       makeKingBBs(),
+		SlidingAttacks: makeSlidingAttackBBs(),
 	}
 }
 
--- board/board_test.go
+++ board/board_test.go
@@ -10,36 +10,36 @@ import (
 func TestNew(t *testing.T) {
 	cb := New()
 
-	if cb.wToMove != 1 {
-		t.Errorf("initial wToMove: want=1, got=%d", cb.wToMove)
+	if cb.WToMove != 1 {
+		t.Errorf("initial wToMove: want=1, got=%d", cb.WToMove)
 	}
 
-	if cb.wPieces != uint64(1)<<16-1 {
-		t.Errorf("initial wPieces: want=65535, got=%d", cb.wPieces)
+	if cb.WPieces != uint64(1)<<16-1 {
+		t.Errorf("initial wPieces: want=65535, got=%d", cb.WPieces)
 	}
-	if cb.wPawns != uint64(1)<<16-1-(uint64(1)<<8-1) {
-		t.Errorf("initial wPawns: want=%b, got=%b", 65279, cb.wPawns)
+	if cb.WPawns != uint64(1)<<16-1-(uint64(1)<<8-1) {
+		t.Errorf("initial wPawns: want=%b, got=%b", 65279, cb.WPawns)
 	}
 
-	wPiecesUnion := cb.wPawns | cb.wRooks | cb.wKnights | cb.wBishops |
-		cb.wQueens | cb.wKing
-	if cb.wPieces != wPiecesUnion {
+	wPiecesUnion := cb.WPawns | cb.WRooks | cb.WKnights | cb.WBishops |
+		cb.WQueens | cb.WKing
+	if cb.WPieces != wPiecesUnion {
 		t.Errorf("wPieces != union of all white pieces. want=65535,\ngot=%b",
 			wPiecesUnion)
 	}
 
 	bPieces := uint64(1)<<63 - 1 + uint64(1)<<63 - (uint64(1)<<48 - 1)
-	if cb.bPieces != bPieces {
-		t.Errorf("initial bPieces: want=%b\n, got=%b", bPieces, cb.bPieces)
+	if cb.BPieces != bPieces {
+		t.Errorf("initial bPieces: want=%b\n, got=%b", bPieces, cb.BPieces)
 	}
 	bPawns := uint64(1)<<56 - 1 - (uint64(1)<<48 - 1)
-	if cb.bPawns != bPawns {
-		t.Errorf("initial bPawns: want=%b\n, got=%b", bPawns, cb.bPawns)
+	if cb.BPawns != bPawns {
+		t.Errorf("initial bPawns: want=%b\n, got=%b", bPawns, cb.BPawns)
 	}
 
-	bPiecesUnion := cb.bPawns | cb.bRooks | cb.bKnights | cb.bBishops |
-		cb.bQueens | cb.bKing
-	if cb.bPieces != bPiecesUnion {
+	bPiecesUnion := cb.BPawns | cb.BRooks | cb.BKnights | cb.BBishops |
+		cb.BQueens | cb.BKing
+	if cb.BPieces != bPiecesUnion {
 		t.Errorf("bPieces != union of all black pieces. want=%b,\ngot=%b",
 			uint64(1)<<63-1+uint64(1)<<63-(uint64(1)<<48-1), bPiecesUnion)
 	}
