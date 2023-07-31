0667de519adf6f3fa1fefe2976e8314283b96475
j1642 <60148902+j1642@users.noreply.github.com>
2023-07-31 12:51:58 -0400

    ENH: Add basic move validation

--- pieces/pieces.go
+++ pieces/pieces.go
@@ -2,7 +2,7 @@ package pieces
 
 import (
 	"engine2/board"
-	_ "fmt"
+	"fmt"
 	_ "math"
 	"math/bits"
 )
@@ -16,8 +16,86 @@ and the opposite for negative directions.
 // TODO: Investigate performance impact of branching in move gen.
 
 func movePiece(from, to int, cb *board.Board) {
-	// Determine piece type.
+	isValid, piece := isValidMove(from, to, cb)
+	if !isValid {
+		fmt.Printf("invalid move for %v: to=%d, from=%d\n", piece, to, from)
+	}
 	// Determine if there is a capture.
+	//toBB := uint64(1<<to)
+	cb.WToMove ^= 1
+}
+
+func isValidMove(from, to int, cb *board.Board) (bool, string) {
+	// Does not check for check, pins, blocking pieces, or legality of a double
+	// pawn push. Castling is currently always invalid.
+	// Use for user-submitted moves only?
+	if from < 0 || from > 63 || to < 0 || to > 63 || to == from {
+		return false, ""
+	}
+	fromBB := uint64(1 << from)
+	toBB := uint64(1 << to)
+	diff := to - from
+	var piece string
+
+	// to == from already excluded, no 0 move bugs from pawnDirections.
+	pawnDirections := [2][8]int{{-7, -8, -9, -16, 0, 0, 0, 0},
+		{7, 8, 9, 16, 0, 0, 0, 0},
+	}
+
+	switch {
+	case fromBB&cb.BwPawns[cb.WToMove] > 0:
+		if !board.ContainsN(diff, pawnDirections[cb.WToMove]) {
+			fmt.Println("invalid move: pawns cannot move like that")
+			return false, "p"
+		}
+		piece = "p"
+	case fromBB&cb.BwKnights[cb.WToMove] > 0:
+		if toBB&cb.NAttacks[from] == 0 {
+			fmt.Println("invalid move: knights cannot move like that")
+			return false, "n"
+		}
+		piece = "n"
+	case fromBB&cb.BwBishops[cb.WToMove] > 0:
+		if toBB&(cb.SlidingAttacks[1][from]|cb.SlidingAttacks[3][from]|
+			cb.SlidingAttacks[5][from]|cb.SlidingAttacks[7][from]) == 0 {
+			fmt.Println("invalid move: bishops cannot move like that")
+			return false, "b"
+		}
+		piece = "b"
+	case fromBB&cb.BwRooks[cb.WToMove] > 0:
+		if toBB&(cb.SlidingAttacks[0][from]|cb.SlidingAttacks[2][from]|
+			cb.SlidingAttacks[4][from]|cb.SlidingAttacks[6][from]) == 0 {
+
+			fmt.Println("invalid move: rooks cannot move like that")
+			return false, "r"
+		}
+		piece = "r"
+	case fromBB&cb.BwQueens[cb.WToMove] > 0:
+		// Combined bishop and rook checks.
+		if toBB&(cb.SlidingAttacks[1][from]|cb.SlidingAttacks[3][from]|
+			cb.SlidingAttacks[5][from]|cb.SlidingAttacks[7][from]|
+
+			cb.SlidingAttacks[0][from]|cb.SlidingAttacks[2][from]|
+			cb.SlidingAttacks[4][from]|cb.SlidingAttacks[6][from]) == 0 {
+
+			fmt.Println("invalid move: queens cannot move like that")
+			return false, "q"
+		}
+		piece = "q"
+	case fromBB&cb.BwKing[cb.WToMove] > 0:
+		if toBB&cb.KAttacks[from] == 0 {
+			fmt.Println("invalid move: kings cannot move like that")
+			return false, "k"
+		}
+		piece = "k"
+	default:
+		fmt.Printf("invalid move: no piece of color %v on that square\n", cb.WToMove)
+		fmt.Printf("wRooks: %b\n", cb.BwRooks[1])
+		fmt.Printf("WtoMove: %b\n", cb.WToMove)
+		fmt.Printf("fromBB: %b\n", fromBB)
+		return false, piece
+	}
+	return true, piece
 }
 
 // Captures and protection are included in move gen.
@@ -93,6 +171,7 @@ func getQueenMoves(square int, cb *board.Board) uint64 {
 
 func getKingMoves(square int, cb *board.Board) uint64 {
 	// TODO: King cannot move to squares attacked by opponent.
+	// TODO: Castling.
 	return cb.KAttacks[square]
 }
 
--- pieces/pieces_test.go
+++ pieces/pieces_test.go
@@ -60,6 +60,144 @@ func TestBishopMoves(t *testing.T) {
 	runMoveGenTests(t, tests)
 }
 
+type validMoveTestCase struct {
+	from, to         int
+	cb               *board.Board
+	expected, actual bool
+}
+
+func runValidMoveTests(t *testing.T, tests []validMoveTestCase) {
+	for _, tt := range tests {
+		if tt.expected != tt.actual {
+			t.Errorf("move %d to %d: want=%v, got=%v",
+				tt.from, tt.to, tt.expected, tt.actual)
+		}
+	}
+}
+
+func firstVal(b bool, s string) bool {
+	return b
+}
+
+func TestIsValidMove(t *testing.T) {
+	cb := board.New()
+	tests := []validMoveTestCase{
+		{
+			from: -1, to: 0,
+			cb:       cb,
+			expected: false,
+			actual:   firstVal(isValidMove(-1, 0, cb)),
+		},
+		{
+			from: 0, to: 100,
+			cb:       cb,
+			expected: false,
+			actual:   firstVal(isValidMove(0, 100, cb)),
+		},
+		// No piece present
+		{
+			from: 20, to: 21,
+			cb:       cb,
+			expected: false,
+			actual:   firstVal(isValidMove(20, 21, cb)),
+		},
+		// Pawn
+		{
+			from: 8, to: 16,
+			cb:       cb,
+			expected: true,
+			actual:   firstVal(isValidMove(8, 16, cb)),
+		},
+		{
+			from: 8, to: 8,
+			cb:       cb,
+			expected: false,
+			actual:   firstVal(isValidMove(8, 8, cb)),
+		},
+		// Knight
+		{
+			from: 1, to: 18,
+			cb:       cb,
+			expected: true,
+			actual:   firstVal(isValidMove(1, 18, cb)),
+		},
+		{
+			from: 6, to: 22,
+			cb:       cb,
+			expected: false,
+			actual:   firstVal(isValidMove(6, 22, cb)),
+		},
+		// Bishop
+		{
+			from: 2, to: 47,
+			cb:       cb,
+			expected: true,
+			actual:   firstVal(isValidMove(2, 47, cb)),
+		},
+		// Bishop passing edge
+		{
+			from: 5, to: 32,
+			cb:       cb,
+			expected: false,
+			actual:   firstVal(isValidMove(5, 32, cb)),
+		},
+		// Rook
+		{
+			from: 0, to: 7,
+			cb:       cb,
+			expected: true,
+			actual:   firstVal(isValidMove(0, 7, cb)),
+		},
+		{
+			from: 7, to: 63,
+			cb:       cb,
+			expected: true,
+			actual:   firstVal(isValidMove(7, 63, cb)),
+		},
+		// Rook passing edge
+		{
+			from: 7, to: 8,
+			cb:       cb,
+			expected: false,
+			actual:   firstVal(isValidMove(7, 8, cb)),
+		},
+		// Queen
+		{
+			from: 3, to: 7,
+			cb:       cb,
+			expected: true,
+			actual:   firstVal(isValidMove(3, 7, cb)),
+		},
+		{
+			from: 7, to: 8,
+			cb:       cb,
+			expected: false,
+			actual:   firstVal(isValidMove(7, 8, cb)),
+		},
+		{
+			from: 7, to: 16,
+			cb:       cb,
+			expected: false,
+			actual:   firstVal(isValidMove(7, 16, cb)),
+		},
+		// King
+		{
+			from: 40, to: 39,
+			cb:       cb,
+			expected: false,
+			actual:   firstVal(isValidMove(40, 39, cb)),
+		},
+		{
+			from: 4, to: 13,
+			cb:       cb,
+			expected: true,
+			actual:   firstVal(isValidMove(4, 13, cb)),
+		},
+	}
+
+	runValidMoveTests(t, tests)
+}
+
 func TestBinSearch(t *testing.T) {
 	nums := [8]int{0, 0, 0, 1, 2, 3, 4, 5}
 	if !binSearch(1, nums) {
