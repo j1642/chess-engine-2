890d21498d6de4ea3fac5a0bfd7a39a517a5b903
j1642 <60148902+j1642@users.noreply.github.com>
2024-02-27 17:20:17 -0500

    MNT: Replace naive count1Bits() with the 10x faster math.bits.OnesCount64() in the std lib

--- engine/engine.go
+++ engine/engine.go
@@ -4,6 +4,7 @@ import (
 	"engine2/board"
 	"engine2/pieces"
 	_ "fmt"
+	"math/bits"
 )
 
 func negamax(alpha, beta, depth int, cb *board.Board) (int, board.Move) {
@@ -46,12 +47,18 @@ func negamax(alpha, beta, depth int, cb *board.Board) (int, board.Move) {
 
 // Return position evaluation in decipawns (0.1 pawns)
 func evaluate(cb *board.Board) int {
-	eval := 10 * (count1Bits(cb.Pawns[1]) - count1Bits(cb.Pawns[0]))
-	eval += 30 * (count1Bits(cb.Knights[1]) - count1Bits(cb.Knights[0]))
-	eval += 31 * (count1Bits(cb.Bishops[1]) - count1Bits(cb.Bishops[0]))
-	eval += 50 * (count1Bits(cb.Rooks[1]) - count1Bits(cb.Rooks[0]))
-	eval += 90 * (count1Bits(cb.Queens[1]) - count1Bits(cb.Queens[0]))
+	// TODO: piece square tables, evaluation tapering (middle to endgame),
+	//   king safety, rooks on (semi-)open files, bishop pair (>= 2),
+	//   endgame rooks/queens on 7th rank, connected rooks,
 
+	eval := 10 * (bits.OnesCount64(cb.Pawns[1]) - bits.OnesCount64(cb.Pawns[0]))
+	eval += 30 * (bits.OnesCount64(cb.Knights[1]) - bits.OnesCount64(cb.Knights[0]))
+	eval += 31 * (bits.OnesCount64(cb.Bishops[1]) - bits.OnesCount64(cb.Bishops[0]))
+	eval += 50 * (bits.OnesCount64(cb.Rooks[1]) - bits.OnesCount64(cb.Rooks[0]))
+	eval += 90 * (bits.OnesCount64(cb.Queens[1]) - bits.OnesCount64(cb.Queens[0]))
+
+	// TODO: outpost squares? Tapering required
+	// TODO: remove knight moves to squares attacked by enemy pawns
 	moveCount := len(pieces.GetAllMoves(cb))
 	cb.WToMove ^= 1
 	oppMoveCount := len(pieces.GetAllMoves(cb))
@@ -77,14 +84,16 @@ func evalPawns(cb *board.Board) int {
 	eval := 0
 	var wPawnsInFile, bPawnsInFile [8]int
 
+	// TODO: replace counting doubled/isolated pawns with an intersection of a
+	// pre-calculated mask (masks[2][64]) and cb.Pawns[WToMove]
 	// Doubled
 	file := uint64(0x101010101010101)
 	for i := 0; i < 8; i++ {
-		wPawnsInFile[i] = count1Bits(file & cb.Pawns[1])
+		wPawnsInFile[i] = bits.OnesCount64(file & cb.Pawns[1])
 		if wPawnsInFile[i] > 1 {
 			eval -= 5 * wPawnsInFile[i]
 		}
-		bPawnsInFile[i] = count1Bits(file & cb.Pawns[0])
+		bPawnsInFile[i] = bits.OnesCount64(file & cb.Pawns[0])
 		if bPawnsInFile[i] > 1 {
 			eval += 5 * bPawnsInFile[i]
 		}
@@ -127,13 +136,4 @@ func evalPawns(cb *board.Board) int {
 	}
 
 	return eval
-}
-
-func count1Bits(bb uint64) int {
-	count := 0
-	for bb > 0 {
-		count += 1
-		bb &= bb - 1
-	}
-	return count
 }
