f594844837d4e6fea8d71007a650839355416a79
j1642 <60148902+j1642@users.noreply.github.com>
2024-03-28 19:06:52 -0400

    MNT: Add TODO comment

--- main.go
+++ main.go
@@ -2,11 +2,11 @@ package main
 
 import (
 	/*
-		    "github.com/j1642/chess-engine-2/pieces"
-		    "math/bits"
-		    "github.com/j1642/chess-engine-2/board"
-	        "fmt"
-	        "time"
+			    "github.com/j1642/chess-engine-2/pieces"
+			    "math/bits"
+			    "github.com/j1642/chess-engine-2/board"
+		        "fmt"
+		        "time"
 	*/
 	"bufio"
 	"github.com/j1642/chess-engine-2/uci"
@@ -14,6 +14,7 @@ import (
 )
 
 func main() {
+	// TODO: add new func pieces.getCaptureMoves(cb) -> []board.Move
 	reader := bufio.NewReader(os.Stdin)
 	for {
 		line, err := reader.ReadString('\n')
